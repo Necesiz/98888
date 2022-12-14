8dd18d96f2baa3744f5c51fa15babdc5a6b805d5
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-14 15:50:30 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
-import os
+Öimport os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5681957815:AAFwaUUAXcY7_-4ZyAnS8_u8HFcmluL1Qls")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 17568815))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "177622d39f23e7c3d015f3d6ebaacd31")
